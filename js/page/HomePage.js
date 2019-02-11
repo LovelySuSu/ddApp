b448f19de2a77fff98730e5ef8ca9d94f50d1ea6
dingding <dingding@dingdingdeMac-mini.local>
2019-02-11 16:32:03 +0800

    tabbar 标题及图标

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -5,21 +5,64 @@ import PopularPage from "./PopularPage";
 import TrendingPage from "./TrendingPage";
 import FavoritePage from "./FavoritePage";
 import MyPage from "./MyPage";
-
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+// react-native-vector-icons安装后要 react-native link react-native-vector-icons
 export default class HomePage extends Component<Props> {
     tabNavigator() {
         return createBottomTabNavigator({
             PopularPage: {
-                screen: PopularPage
+                screen: PopularPage,
+                navigationOptions: {
+                    tabBarLabel: '最热',
+                    tabBarIcon: ({tintColor,focused}) => {
+                        return <FontAwesome5
+                            name={'hotjar'}
+                            size={26}
+                            style={{color: tintColor}}
+                        />
+                    }
+                }
             },
             TrendingPage: {
-                screen: TrendingPage
+                screen: TrendingPage,
+                navigationOptions: {
+                    tabBarLabel: '趋势',
+                    tabBarIcon: ({tintColor,focused}) => {
+                        return <Ionicons
+                            name={'md-trending-up'}
+                            size={26}
+                            style={{color: tintColor}}
+                        />
+                    }
+                }
             },
             FavoritePage: {
-                screen: FavoritePage
+                screen: FavoritePage,
+                navigationOptions: {
+                    tabBarLabel: '收藏',
+                    tabBarIcon: ({tintColor,focused}) => {
+                        return <MaterialIcons
+                            name={'favorite'}
+                            size={26}
+                            style={{color: tintColor}}
+                        />
+                    }
+                }
             },
             MyPage: {
-                screen: MyPage
+                screen: MyPage,
+                navigationOptions: {
+                    tabBarLabel: '我的',
+                    tabBarIcon: ({tintColor,focused}) => {
+                        return <MaterialIcons
+                            name={'person'}
+                            size={26}
+                            style={{color: tintColor}}
+                        />
+                    }
+                }
             }
         })
     }
