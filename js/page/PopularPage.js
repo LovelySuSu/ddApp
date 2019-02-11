19fe841399402e12387ec9ab17c4e2271084b261
dingding <dingding@dingdingdeMac-mini.local>
2019-02-11 18:57:20 +0800

    动态设置顶部tab及样式

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -7,20 +7,34 @@ import {
 import { createMaterialTopTabNavigator } from 'react-navigation'
 import NavigationUtil from "../navigator/NavigationUtil";
 export default class PopularPage extends Component<Props> {
+    constructor(props){
+        super(props)
+        this.tabNames = ['JS','React','IOS','React Native','Vue']
+    }
+    genTabs() {
+        const tabs = {}
+        this.tabNames.forEach((item,index)=>{
+            tabs[`tab${index}`] = {
+                screen: PopularTab,
+                navigationOptions:{
+                    title: item
+                }
+            }
+        })
+        return tabs
+    }
     render() {
-        const TabTopNavigator = createMaterialTopTabNavigator({
-            PopularTab1: {
-                screen: PopularTab,
-                navigationOptions:{
-                    title: 'tab1'
-                }
-            },
-            PopularTab2: {
-                screen: PopularTab,
-                navigationOptions:{
-                    title: 'tab2'
-                }
-            },
+        const TabTopNavigator = createMaterialTopTabNavigator(this.genTabs(),{
+            tabBarOptions: {
+                tabStyle: styles.tabStyle,
+                upperCaseLabel: false, // 是否使用标签大写，默认为true
+                scrollEnabled: true, // 是否支持选项卡滚动，默认为false
+                style: {
+                    backgroundColor: '#678' // tabBar 背景颜色
+                },
+                indicatorStyle: styles.indicatorStyle, // 标签指示器的样式
+                labelStyle: styles.labelStyle
+            }
         })
         return (
             <View style={{ flex: 1,marginTop: 30 }}>
@@ -48,9 +62,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
+    tabStyle: {
+        minWidth: 50,
+    },
+    indicatorStyle: {
+        height: 2,
+        backgroundColor:'#fff'
+    },
+    labelStyle: {
+        fontSize: 13,
+        marginVertical: 6,
         textAlign: 'center',
-        margin: 10,
     }
 });
