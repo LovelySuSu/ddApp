720aeef3d7117bb38f4a9933627b655eedba6663
dingding <dingding@dingdingdeMac-mini.local>
2019-02-14 11:58:21 +0800

    我的页面使用自定义navigationBar 以及头部左右按钮

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,13 +1,69 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TouchableOpacity
+} from 'react-native';
 import actions from "../action";
 import { connect } from "react-redux";
+import NavigationBar from "../common/NavigationBar";
+import { THEME_COLOR } from "../constant";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 
 class MyPage extends Component<Props> {
+    getRightButton() {
+        return (
+            <TouchableOpacity
+                onPress={() => {}}
+            >
+                <View
+                    style={{ padding: 5,marginRight: 8 }}
+                >
+                    <AntDesign
+                        name={'search1'}
+                        size={24}
+                        color={'#fff'}
+                    />
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    getLeftButton(callBack) {
+        return (
+            <TouchableOpacity
+                onPress={callBack}
+            >
+                <View
+                    style={{ padding: 5,paddingLeft: 22 }}
+                >
+                    <Ionicons
+                        name={'ios-arrow-back'}
+                        size={26}
+                        color={'#fff'}
+                    />
+                </View>
+            </TouchableOpacity>
+        )
+    }
     render() {
+        let statusBar = {
+            backgroundColor: THEME_COLOR,
+            barStyle: 'light-content'
+        }
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>My Page!</Text>
+                <NavigationBar
+                    title={'我的'}
+                    statusBar={statusBar}
+                    style={{ backgroundColor: THEME_COLOR }}
+                    leftButton={this.getLeftButton(()=>{})}
+                    rightButton={this.getRightButton()}
+                />
+                <Text style={styles.welcome}>My Page</Text>
                 <Button
                     title={'改变主题颜色'}
                     onPress={() => this.props.onThemeChange('orange')}
@@ -20,9 +76,6 @@ class MyPage extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
     },
     welcome: {
         fontSize: 20,
